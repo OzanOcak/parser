407737067b65db2b0eef139bfed08fbcdf0c6ccd
OzanOcak <ozanocak@gmail.com>
2023-11-05 10:52:19 -0500

    passing state and deconstruction

--- section01.js
+++ section01.js
@@ -1,15 +1,21 @@
-const str = (s) => (targetString) => {
-  if (targetString.startsWith(s)) {
-    return targetString;
+const str = (s) => (parserState) => {
+  // initially index is 0
+  const { targetString, index } = parserState;
+  if (targetString.slice(index).startsWith(s)) {
+    return { ...parserState, result: s, index: index + s.length }; // if they're equal , result is s
   }
   throw new Error(
-    `Tried to match "${s}", but found "${targetString.slice(0, 10)}`
+    `Tried to match "${s}", but found "${targetString.slice(index, index + 10)}`
   );
 };
 
 const run = (parser, targetString) => {
-  //str assigns parser &  parse get the second argument
-  return parser(targetString);
+  const initialState = {
+    targetString,
+    index: 0,
+    result: null,
+  };
+  return parser(initialState);
 };
 
 const parser = str("hello!"); //  str get first argument
