c2c034a2b3bfc1064273853a2c4ea89660cefd3d
OzanOcak <ozanocak@gmail.com>
2023-11-07 12:42:21 -0500

    letter and digit parser

--- section01.js
+++ section01.js
@@ -66,11 +66,74 @@ const str = (s) =>
       return updateParserState(parserState, index + s.length, s);
     }
 
+    return updateParserError(parserState, `letters: Got unexpected error`);
+  });
+
+const lettersRegex = /^[A-Za-z]+/;
+const letters = new Parser((parserState) => {
+  const { targetString, index, isError } = parserState;
+
+  if (isError) {
+    return parserState;
+  }
+
+  const slicedTarget = targetString.slice(index);
+
+  if (slicedTarget.length === 0) {
     return updateParserError(
       parserState,
-      `Tried to match "${s}", but got "${targetString.slice(index, index + 10)}`
+      `letters: Got Unexpected end of input.`
     );
-  });
+  }
+
+  const regexMatch = slicedTarget.match(lettersRegex);
+
+  if (regexMatch) {
+    return updateParserState(
+      parserState,
+      index + regexMatch[0].length,
+      regexMatch[0]
+    );
+  }
+
+  return updateParserError(
+    parserState,
+    `letters: Couldn't match letters at index ${index}`
+  );
+});
+
+const digitsRegex = /^[0-9]+/;
+const digits = new Parser((parserState) => {
+  const { targetString, index, isError } = parserState;
+
+  if (isError) {
+    return parserState;
+  }
+
+  const slicedTarget = targetString.slice(index);
+
+  if (slicedTarget.length === 0) {
+    return updateParserError(
+      parserState,
+      `digits: Got Unexpected end of input.`
+    );
+  }
+
+  const regexMatch = slicedTarget.match(digitsRegex);
+
+  if (regexMatch) {
+    return updateParserState(
+      parserState,
+      index + regexMatch[0].length,
+      regexMatch[0]
+    );
+  }
+
+  return updateParserError(
+    parserState,
+    `digits: Couldn't match digits at index ${index}`
+  );
+});
 
 const sequenceOf = (parsers) =>
   new Parser((parserState) => {
@@ -88,7 +151,15 @@ const sequenceOf = (parsers) =>
     return updateParserResult(nextState, results);
   });
 
-const parser = str("hello!")
+const parser = sequenceOf([digits, letters, digits]);
+
+console.log(parser.run("123abc456"));
+
+//const parser = letters;
+
+//console.log(parser.run("abc12345"));
+
+/*const parser = str("hello!")
   .map((result) => ({
     value: result.toUpperCase(),
   }))
